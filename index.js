cbcc781e826af6e333f42badd03f2e8021e04e44
iwin-haha <119930197+iwin-haha@users.noreply.github.com>
2022-12-17 17:55:16 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ function main(){
     });    
 }
 
+const config = {
+    // etc...
+    kit: {
+      // etc...
+      floc: process.env.NODE_ENV === "development",
+    },
+  };
+  
 main()
 
 async function send(e){     
